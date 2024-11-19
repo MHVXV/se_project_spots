69fbcacc698f32e6f80b309df27f0420ea087f82
Clarke <clarke.brodnicki@gmail.com>
2024-11-19 10:39:07 -0800

    fix image width in mobile view, fix class of mocdal close buttons

--- scripts/index.js
+++ scripts/index.js
@@ -52,7 +52,7 @@ const previewModal = document.querySelector("#preview-modal");
 const previewModalImageEl = previewModal.querySelector(".modal__image");
 const previewModalCaptionEl = previewModal.querySelector(".modal__caption");
 
-const previewModalCloseButton = previewModal.querySelector(".modal__close");
+const previewModalCloseButton = previewModal.querySelector(".modal__close-btn_type_preview");
 
 const cardTemplate = document.querySelector("#card-template");
 const cardsList = document.querySelector(".cards__list");
