e58ff4ae9f1107653d5d851252439db9f6c14d7a
Clarke <clarke.brodnicki@gmail.com>
2025-02-14 07:19:31 -0800

    remove default error msg from index.html

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,6 @@ import {
 } from "../scripts/validation.js";
 import Api from "../utils/Api.js";
 import { setButtonText, setDeleteButtonText } from "../utils/Helpers.js";
-// import { renderLoading, handleSubmit } from "../utils/Utils.js";
 
 // const initialCards = [
 //   {
@@ -75,7 +74,6 @@ const profileDescription = document.querySelector(".profile__description");
 
 const editModal = document.querySelector("#profile-edit-modal");
 const profileForm = document.forms["profile-form"];
-const editModalCloseButton = editModal.querySelector(".modal__close-btn");
 const editModalNameInput = editModal.querySelector("#profile-name-input");
 const editModalDescriptionInput = editModal.querySelector(
   "#profile-description-input"
@@ -84,9 +82,7 @@ const editModalDescriptionInput = editModal.querySelector(
 // Avatar form elements
 const avatarModal = document.querySelector("#avatar-modal");
 const avatarForm = avatarModal.querySelector("#edit-avatar-form");
-const avatarModalCloseButton = avatarModal.querySelector(".modal__close-btn");
 const avatarModalSubmitButton = avatarModal.querySelector(".modal__submit-btn");
-const avatarSaveButton = avatarModal.querySelector(".modal__submit_edits-btn");
 const avatarInput = avatarModal.querySelector("#profile-avatar-input");
 
 // Delete form elements
@@ -97,18 +93,12 @@ const cancelBtn = deleteModal.querySelector(".modal__cancel-btn");
 // Card form elements
 const cardModal = document.querySelector("#add-card-modal");
 const cardForm = document.forms["card-form"];
-const cardModalCloseButton = cardModal.querySelector(".modal__close-btn");
-const cardModalSubmitButton = cardModal.querySelector(".modal__submit-btn");
 const cardNameInput = cardModal.querySelector("#add-card-name-input");
 const cardLinkInput = cardModal.querySelector("#add-card-link-input");
 
 const previewModal = document.querySelector("#preview-modal");
 const previewModalImageEl = previewModal.querySelector(".modal__image");
 const previewModalCaptionEl = previewModal.querySelector(".modal__caption");
-
-const previewModalCloseButton = previewModal.querySelector(
-  ".modal__close-btn_type_preview"
-);
 
 let selectedCard, selectedCardId;
 
